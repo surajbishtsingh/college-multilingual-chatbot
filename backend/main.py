75393ade0958454aca27b7f9a3e0e92176e9e1f7
surajbishtsingh <bishtsuraj0311@gmail.com>
2026-05-05 11:27:56 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -134,7 +134,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=False,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 
